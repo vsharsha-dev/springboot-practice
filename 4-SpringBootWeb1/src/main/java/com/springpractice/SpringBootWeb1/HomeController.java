c8b10a454e480a04b335d3648537ae329227c80c
vsharsha <vsharsha48@gmail.com>
2024-12-26 23:26:09 +0530

    Remove session and add Model object

--- 4-SpringBootWeb1/src/main/java/com/springpractice/SpringBootWeb1/HomeController.java
+++ 4-SpringBootWeb1/src/main/java/com/springpractice/SpringBootWeb1/HomeController.java
@@ -3,6 +3,7 @@ package com.springpractice.SpringBootWeb1;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,12 +17,16 @@ public class HomeController {
     }
 
     @RequestMapping("/add")
-    public String add(@RequestParam("num1") int number1, @RequestParam("num2") int number2, HttpSession session) {
+    public String add(@RequestParam("num1") int number1, @RequestParam("num2") int number2, Model model) {
+//        int num1 = Integer.parseInt(req.getParameter("num1"));
+//        int num2 = Integer.parseInt(req.getParameter("num2"));
 
         int result = number1+number2;
 
-        // This session can be accessed directly in result.jsp
-        session.setAttribute("result", result);
+        model.addAttribute("result", result);
+
+//        // This session can be accessed directly in result.jsp
+//        session.setAttribute("result", result);
 
         return "result.jsp";
     }
