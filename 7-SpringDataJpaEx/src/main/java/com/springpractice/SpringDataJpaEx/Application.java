c78153c55990b631292985b900d3ffffb420bb28
vsharsha <vsharsha48@gmail.com>
2024-12-31 23:02:36 +0530

    Modify SpringDataJpaEx files for query operations

--- 7-SpringDataJpaEx/src/main/java/com/springpractice/SpringDataJpaEx/Application.java
+++ 7-SpringDataJpaEx/src/main/java/com/springpractice/SpringDataJpaEx/Application.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 @SpringBootApplication
 public class Application {
 
@@ -18,19 +21,26 @@ public class Application {
 		Student s2 = context.getBean(Student.class);
 		Student s3 = context.getBean(Student.class);
 
-		s1.setRollNo(1);
-		s1.setName("Sai");
-		s1.setMarks(50);
+//		s1.setRollNo(1);
+//		s1.setName("Sai");
+//		s1.setMarks(50);
+//
+//		s2.setRollNo(2);
+//		s2.setName("Harsha");
+//		s2.setMarks(70);
+//
+//		s3.setRollNo(3);
+//		s3.setName("Kiran");
+//		s3.setMarks(90);
+//
+//		repo.save(s2);
+//		repo.save(s3);
 
-		s2.setRollNo(2);
-		s2.setName("Harsha");
-		s2.setMarks(70);
+//		Optional<Student> s = repo.findById(100); // findById returns "Optional" object which can be null
+//
+//		System.out.println(s.orElse(new Student())); // If s is null, it prints new Student object
 
-		s3.setRollNo(3);
-		s3.setName("Kiran");
-		s3.setMarks(90);
-
-		repo.save(s1);
+		System.out.println(repo.findByName("Harsha"));
 	}
 
 }
--- 7-SpringDataJpaEx/src/main/java/com/springpractice/SpringDataJpaEx/StudentRepo.java
+++ 7-SpringDataJpaEx/src/main/java/com/springpractice/SpringDataJpaEx/StudentRepo.java
@@ -2,8 +2,14 @@ package com.springpractice.SpringDataJpaEx;
 
 import com.springpractice.SpringDataJpaEx.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StudentRepo extends JpaRepository<Student, Integer> {
+
+    @Query("select s from Student s where s.name = ?1")
+    List<Student> findByName(String name);
 }
