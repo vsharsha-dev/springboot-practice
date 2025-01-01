27a584d2936a0ef2274cb5ebaf3b8248fe6147b3
vsharsha <vsharsha48@gmail.com>
2025-01-01 23:01:33 +0530

    Add initial SpringDataRest files

---
+++ 9-SpringDataRest/src/main/java/com/springpractice/SpringDataRest/model/JobPost.java
@@ -0,0 +1,28 @@
+package com.springpractice.SpringDataRest.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+@Entity
+public class JobPost {
+
+	@Id
+	private int postId;
+	private String postProfile; 
+	private String postDesc;
+	private Integer reqExperience;
+	private List<String> postTechStack;
+	
+
+}
---
+++ 9-SpringDataRest/src/main/java/com/springpractice/SpringDataRest/repo/JobRepo.java
@@ -0,0 +1,13 @@
+package com.springpractice.SpringDataRest.repo;
+
+import com.springpractice.SpringDataRest.model.JobPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface JobRepo extends JpaRepository<JobPost, Integer> {
+
+	// List<JobPost> findByPostProfileContainingOrPostDescContaining(String postProfile, String postDesc);
+}
