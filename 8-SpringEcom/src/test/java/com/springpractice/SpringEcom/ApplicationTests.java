d038f16980b089a39758a2be33489c7c762cb45d
vsharsha <vsharsha48@gmail.com>
2025-01-01 17:51:20 +0530

    Add SpringEcom ecommerce project first commit

---
+++ 8-SpringEcom/src/test/java/com/springpractice/SpringEcom/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springpractice.SpringEcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
